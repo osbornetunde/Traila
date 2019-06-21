3f41dbdf98e8ae2e636606fee7713d3897ce9f1f
Tunde Osborne <osbornetunde@gmail.com>
2019-06-21 12:10:59 +0100

    Updates

---
+++ precache-manifest.5d1035a7c44cb266824cd97b87e402ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc41c0a17ea4443ff7cd2dd91b2d5888",
+    "url": "/traila/index.html"
+  },
+  {
+    "revision": "48f85b76c8f1d06ef761",
+    "url": "/traila/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "e016f6454717527496db",
+    "url": "/traila/static/css/main.19c361b0.chunk.css"
+  },
+  {
+    "revision": "48f85b76c8f1d06ef761",
+    "url": "/traila/static/js/2.fed09927.chunk.js"
+  },
+  {
+    "revision": "e016f6454717527496db",
+    "url": "/traila/static/js/main.0f08fd14.chunk.js"
+  },
+  {
+    "revision": "341262a244b35f1deeda",
+    "url": "/traila/static/js/runtime~main.49a12fef.js"
+  }
+]);
